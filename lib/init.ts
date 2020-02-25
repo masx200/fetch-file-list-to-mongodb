e050a8bbda702b11ee689f10b08de567c8fe505a
masx200 <34191203+masx200@users.noreply.github.com>
2020-02-25 09:20:43 +0800

    Update init.ts

--- lib/init.ts
+++ lib/init.ts
@@ -8,7 +8,7 @@ async function savecookies(setcookie: string) {
 const cooobj={}
 
 //可能有多个set-cookie
-     setcookie.split(",").forEach(coo=>Object.assign(cooobj,cookie.parse(coo)) 
+     setcookie.split(",").forEach(coo=>Object.assign(cooobj,cookie.parse(coo)) )
 
 
     const panobj = await fsextra.readJSON(jsonfile);
