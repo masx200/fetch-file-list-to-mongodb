6f3746cb3fcd26cdb7e2eef0fe009704fa5fe93e
masx200 <masx200@qq.com>
2020-02-26 00:38:08 +0800

    美化，保存到数据库改用reduce,防止内存溢出

--- lib/savetodb.ts
+++ lib/savetodb.ts
@@ -14,21 +14,31 @@ export default async function(fileslist: Array<PANFILE | PANDIR>, dir: string) {
     // const savepro1 = panFilecollect.updateMany(,files.map(mapfileobjdir),);
     // const savepro2 = panDircollect.updateMany(dirs.map(mapfileobjdir));
     // [options.upsert = false] «布尔»如果为true，并且找不到文档，请插入新文档
-    const savefilepro = filetosave.map(obj => {
+    //防止内存溢出   把map改为reduce
+    const savefilepro = filetosave.reduce(async (prev, obj) => {
+        await prev;
         return panFilecollect
             .updateMany({ path: obj.path }, obj, {
                 upsert: true
             })
             .exec();
-    });
-    const savedirpro = dirtosave.map(obj => {
+    }, Promise.resolve());
+    const savedirpro = dirtosave.reduce(async (prev, obj) => {
+        await prev;
         return panDircollect
             .updateMany({ path: obj.path }, obj, {
                 upsert: true
             })
             .exec();
-    });
-    await Promise.all([...savefilepro, ...savedirpro]);
+    }, Promise.resolve());
+    /* const savedirpro = dirtosave.map(async obj => {
+        return panDircollect
+            .updateMany({ path: obj.path }, obj, {
+                upsert: true
+            })
+            .exec();
+    });*/
+    await Promise.all([savefilepro, savedirpro]);
     /* 如果有已经保存过目录的信息,则设置finished 为true */
     // await panDircollect.updateMany({ path: dir }, { finished: true });
     // .find({ path: dir })
