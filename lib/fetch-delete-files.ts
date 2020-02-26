1a96b03a26b30e6b534a5fea1065786a8ab522a9
masx200 <34191203+masx200@users.noreply.github.com>
2020-02-26 23:40:52 +0800

    添加错误重试

--- lib/fetch-delete-files.ts
+++ lib/fetch-delete-files.ts
@@ -27,54 +27,69 @@ async function fetchdelete(filestoremove: string[]): Promise<any[]> {
         let coostr = objtostrcookie(panobj);
         PANENV.cookie = coostr;
     }
-    const params = {
-        opera: "delete",
-        async: "1",
-        onnest: "fail",
-        channel: "chunlei",
-        web: "1",
-        app_id: "250528",
-        bdstoken: PANENV.bdstoken,
-        logid: PANENV.logid,
-        clienttype: "0"
-    };
-    const listapi = new URL(operationurl);
-    listapi.search = String(new URLSearchParams(params));
-    const urlhref = String(listapi);
-    const body =
-        "filelist=" + encodeURIComponent(JSON.stringify(filestoremove));
-    const headers = {
-        Host: "pan.baidu.com",
-        Connection: "keep-alive",
+    try {
+        const params = {
+            opera: "delete",
+            async: "1",
+            onnest: "fail",
+            channel: "chunlei",
+            web: "1",
+            app_id: "250528",
+            bdstoken: PANENV.bdstoken,
+            logid: PANENV.logid,
+            clienttype: "0"
+        };
+        const listapi = new URL(operationurl);
+        listapi.search = String(new URLSearchParams(params));
+        const urlhref = String(listapi);
+        const body =
+            "filelist=" + encodeURIComponent(JSON.stringify(filestoremove));
+        const headers = {
+            Host: "pan.baidu.com",
+            Connection: "keep-alive",
 
-        Accept: "application/json, text/javascript, */*; q=0.01",
-        "X-Requested-With": "XMLHttpRequest",
-        "User-Agent": `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.26 Safari/537.36 Edg/81.0.416.16`,
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        Origin: "https://pan.baidu.com",
-        "Sec-Fetch-Site": "same-origin",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Dest": "empty",
-        Referer: " https://pan.baidu.com/disk/home?",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-        Cookie: PANENV.cookie
-    };
-    const req = await fetch(urlhref, { method: "POST", body, headers });
-    if (req.ok) {
-        const data = await req.json();
-        const info = data?.info;
-        if (Array.isArray(info) && info.length) {
-            console.log(info);
-            return info;
+            Accept: "application/json, text/javascript, */*; q=0.01",
+            "X-Requested-With": "XMLHttpRequest",
+            "User-Agent": `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.26 Safari/537.36 Edg/81.0.416.16`,
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            Origin: "https://pan.baidu.com",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Dest": "empty",
+            Referer: " https://pan.baidu.com/disk/home?",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language":
+                "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+            Cookie: PANENV.cookie
+        };
+        const req = await fetch(urlhref, { method: "POST", body, headers });
+        if (req.ok) {
+            const data = await req.json();
+            const info = data?.info;
+            if (Array.isArray(info) && info.length) {
+                console.log(info);
+                return info;
+            } else {
+                throw Error("data error " + JSON.stringify(data));
+            }
+            // return data;
         } else {
-            throw Error("data error " + JSON.stringify(data));
+            throw Error(
+                "fetch failed " +
+                    req.status +
+                    " " +
+                    req.statusText +
+                    " " +
+                    urlhref
+            );
         }
-        // return data;
-    } else {
-        throw Error(
-            "fetch failed " + req.status + " " + req.statusText + " " + urlhref
-        );
+    } catch (e) {
+        console.error("删除文件错误,5秒后重试.");
+        console.error(e);
+        await new Promise(r => {
+            setTimeout(r, 5000);
+        });
+        return fetchdelete(filestoremove);
     }
 }
 export async function deletefiles(rawfiles: Array<string>): Promise<void> {
