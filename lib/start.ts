b7368414fb40032764db94789ea54617cd762ffa
masx200 <masx200@qq.com>
2020-06-06 20:19:42 +0800

    Update start.ts

--- lib/start.ts
+++ lib/start.ts
@@ -1,24 +1,24 @@
 const homepath = `/`;
-import { initPANENV } from "@masx200/fetch-baidu-pan-files";
+// import { initPANENV } from "@masx200/fetch-baidu-pan-files";
 import mongoose from "mongoose";
 import process from "process";
 // import { homepath } from "./index.js";
 import { listandsave } from "./listandsave.js";
 
 export async function start() {
-    const panenv = await initPANENV();
+    // const panenv = await initPANENV();
     const connection = mongoose.connect("mongodb://127.0.0.1/", {
         poolSize: 10,
         useUnifiedTopology: true,
         useNewUrlParser: true,
         useCreateIndex: true,
-        dbName: "pan_" + panenv.user
+        dbName: "baidupan"
     });
     connection.then(() => {
         console.log("mongodb conneted");
     });
     console.log("登陆成功");
-    console.log(JSON.stringify(panenv));
+    // console.log(JSON.stringify(panenv));
     await listandsave(homepath /* , bdstoken, logid */);
     console.log("文件数据库全部建立完成");
     process.exit();
