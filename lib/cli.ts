00206a086ec71c5af25f41833a426b0c1ed228c9
masx200 <masx200@qq.com>
2024-10-28 14:59:59 +0800

    "version": "2.0.5",

--- lib/cli.ts
+++ lib/cli.ts
@@ -3,7 +3,9 @@
 import parseargs from "@masx200/mini-cli-args-parser";
 import opts from "./opts.js";
 import { start } from "./start.js";
-
+process.on("unhandledRejection", (err) => {
+    throw err;
+});
 const args = parseargs(process.argv.slice(2));
 console.log(args);
 if (args["collect"]) {
